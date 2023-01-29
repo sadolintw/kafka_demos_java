83e9477fd30c014e6dab327d10984ee7a12106fc
sado <sadolintw@gmail.com>
2023-01-29 13:14:05 +0800

    init

---
+++ src/main/java/org/example/kafka/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.example.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
+    public static void main(String[] args) {
+        log.info("HelloWorld");
+    }
+}
